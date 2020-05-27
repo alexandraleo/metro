bcff6b6c64972ae2df2a0da5b4074aa9f945640d
Alexandra <kabrenok@gmail.com>
2020-05-27 18:10:57 +0300

    News in obj

--- src/components/news.js
+++ src/components/news.js
@@ -1,13 +1,9 @@
 import Component from "../component.js";
 
-const createNewsTemplate = () => {
-  return (
-    `<div class="news-cards"></div>`
-  );
-};
-
 export default class News extends Component {
   getTemplate() {
-    return createNewsTemplate();
+    return (
+      `<div class="news-cards"></div>`
+    );
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 // import Hello from "./components/hello-page.js";
+import Menu from "./components/menu.js";
 import Word from "./components/word.js";
 import {render, RenderPosition} from "./render.js";
-import {news} from "./news-data.js";
+import {news} from "./data/news-data.js";
 
 // let helloNode = document.querySelector(`.hello-page`);
+let menuNode = document.querySelector(`.page-header__navigation`);
 let newsNode = document.querySelector(`.news-page__board`);
 
+const renderMenu = () => {
+  const menuElement = new Menu();
+  render(menuNode, menuElement, RenderPosition.AFTERBEGIN);
+};
 // const renderHelloPage = () => {
 //   const helloElement = new Hello();
 //   render(helloNode, helloElement, RenderPosition.BEFOREEND);
@@ -16,6 +22,7 @@ const renderNews = () => {
     render(newsNode, wordElement, RenderPosition.AFTERBEGIN);
   }
 };
-
 // renderHelloPage();
+renderMenu();
 renderNews();
+console.log(document.querySelector(`.navigation-list--news`));
